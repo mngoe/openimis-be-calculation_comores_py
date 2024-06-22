0cd9708b295d116520727bd374a1756b28a1220e
kevel <keveltiah@gmail.com>
2024-06-22 20:50:03 +0100

    feta: Ajout des champs de type parametres ref28941

--- calculation_comores/config.py
+++ calculation_comores/config.py
@@ -1,4 +1,78 @@
-CLASS_RULE_PARAM_VALIDATION = []
+CLASS_RULE_PARAM_VALIDATION = [
+    {
+        "class": "ContributionPlan",
+        "parameters": [
+            {
+                "type": "number",
+                "name": "childsum",
+                "label": {
+                    "en": "Child sum",
+                    "fr": "Montant pour enfant"
+                },
+                "rights": {
+                    "read": "150201",
+                    "write": "150202",
+                    "update": "150203",
+                    "replace": "150206",
+                },
+                "relevance": "True",
+                # "condition": "INPUT>100",
+                "default": "0"
+            },
+            {
+                "type": "number",
+                "name": "adultmalesum",
+                "label": {
+                    "en": "Amount for adult man",
+                    "fr": "Montant pour homme adulte"
+                },
+                "rights": {
+                    "read": "150201",
+                    "write": "150202",
+                    "update": "150203",
+                    "replace": "150206",
+                },
+                "relevance": "True",
+                # "condition": "INPUT>100",
+                "default": "0"
+            },
+            {
+                "type": "number",
+                "name": "adultfemalesum",
+                "label": {
+                    "en": "Amount for adult woman",
+                    "fr": "Montant pour femme adulte"
+                },
+                "rights": {
+                    "read": "150201",
+                    "write": "150202",
+                    "update": "150203",
+                    "replace": "150206",
+                },
+                "relevance": "True",
+                # "condition": "INPUT>100",
+                "default": "0"
+            },
+            {
+                "type": "number",
+                "name": "lumpsum",
+                "label": {
+                    "en": "Lump sum",
+                    "fr": "Montant forfaitaire"
+                },
+                "rights": {
+                    "read": "150201",
+                    "write": "150202",
+                    "update": "150203",
+                    "replace": "150206",
+                },
+                "relevance": "True",
+                # "condition": "INPUT>100",
+                "default": "0"
+            }
+        ],
+    },
+]
 
 FROM_TO = [
         {"from": "Policy", "to": "Invoice"},
